5271aab7df744fce643c3d5003db2ef3b9a99e86
Ward keadan <73485499+wardkadan1@users.noreply.github.com>
2024-09-04 14:24:22 +0300

    Understading_Let_const_var.4

---
+++ javascript/Understading_Let_const_var.4/Understading_Let_const_var.4.js
@@ -0,0 +1,39 @@
+//1
+
+var myNumber = 42;
+console.log(myNumber);
+
+//2
+
+let myString = "Hello, World!";
+console.log(myString);
+
+//3
+
+const isJavaScriptFun = true;
+console.log(isJavaScriptFun);
+
+//4
+
+var age = 25;
+console.log(age);
+
+age = 30;
+console.log(age);
+
+//5
+
+let city = "Paris";
+console.log(city);
+
+city = "London";
+console.log(city);
+
+//6
+
+const pi = 3.14;
+console.log(pi);
+
+pi = 3.14159;
+
+console.log(pi);
