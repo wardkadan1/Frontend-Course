15d0761fd62b1474a305afcc929aca0b8f4d9445
Ward keadan <73485499+wardkadan1@users.noreply.github.com>
2024-09-04 15:54:44 +0300

    String_concatenation.7

--- javascript/String_concatenation.7/String_concatenation.7.js
+++ javascript/String_concatenation.7/String_concatenation.7.js
@@ -1,9 +1,9 @@
 //1
 
-const name = "ward";
+const name1 = "ward";
 const age = 24;
 
-console.log("My name is " + name + " and I am " + age + " years old.");
+console.log("My name is " + name1 + " and I am " + age + " years old.");
 
 //2
 
