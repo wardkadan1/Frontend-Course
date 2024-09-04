328c421b7bbf6907bd6d3ec3d2f09086ded24e78
Ward keadan <73485499+wardkadan1@users.noreply.github.com>
2024-09-04 15:43:02 +0300

    String_concatenation.7

---
+++ javascript/String_concatenation.7/String_concatenation.7.js
@@ -0,0 +1,36 @@
+//1
+
+const name = "ward";
+const age = 24;
+
+console.log("My name is " + name + " and I am " + age + " years old.");
+
+//2
+
+const favoriteColor = "blue";
+const favoriteMovie = "Iron Man";
+const favoriteFood = "pizza";
+
+console.log(
+  "My favorite color is " +
+    favoriteColor +
+    ", I love the movie '" +
+    favoriteMovie +
+    "', and I enjoy eating " +
+    favoriteFood +
+    "."
+);
+
+//3
+
+const a = 5;
+const b = 10;
+const result = a + b;
+
+console.log("The sum of a and b is " + result);
+
+//4
+
+const day = "Monday";
+
+console.log("Today is " + day);
