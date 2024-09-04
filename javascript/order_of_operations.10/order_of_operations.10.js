298e82a15adbad879a2f276292d0288c024ff459
Ward keadan <73485499+wardkadan1@users.noreply.github.com>
2024-09-04 16:50:22 +0300

    order_of_operations.10

---
+++ javascript/order_of_operations.10/order_of_operations.10.js
@@ -0,0 +1,114 @@
+//1
+
+const a = 8;
+const b = 2;
+const result = a * b + 3;
+console.log(result);
+
+//2
+
+const x = 4;
+const y = 2;
+const z = 3;
+const result = x + y * z - 1;
+console.log(result);
+
+//3
+
+const a = 4;
+const b = 2;
+const result = (a + b) * (b - 1);
+console.log(result); // Output: 6
+
+//4
+
+const num = 50;
+const x = 5;
+const y = 6;
+const result = num - x * y;
+console.log(result);
+
+//5
+
+const a = 8;
+const b = 2;
+const c = 4;
+const result = (a / b) * c;
+console.log(result);
+
+//6
+
+const a = 2;
+const b = 3;
+const c = 4;
+
+const result1 = a + b * c;
+console.log(result1);
+
+const result2 = (a + b) * c;
+console.log(result2);
+
+//7
+
+const a = 2;
+const b = 3;
+const c = 5;
+const result = ((a + b) * c) / 2;
+console.log(result);
+
+//8
+
+const a = 3;
+const b = 6;
+const c = 5;
+const d = 4;
+
+const result = a + (b * (c + d)) / b - d;
+console.log(result);
+
+//9
+
+const num = 2;
+const str = "3";
+const result = num + str * 4;
+console.log(result);
+
+//10
+
+const a = 10;
+const b = 20;
+const c = "30";
+
+const result1 = a + b + c;
+console.log(result1);
+
+const result2 = c + a + b;
+console.log(result2);
+
+//11
+
+const numStr = "3";
+const num = 10;
+
+const result = +numStr + num;
+console.log(result);
+
+//12
+
+let x = 10;
+const result = x++ * 2;
+console.log(result);
+console.log(x);
+
+//13
+
+const a = 10;
+const result = a / 0;
+console.log(result);
+
+//14
+
+const a = 15;
+const b = 4;
+const result = a % b;
+console.log(result);
