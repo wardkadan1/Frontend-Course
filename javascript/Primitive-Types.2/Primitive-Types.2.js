8e42ec473f7ac0e00db442040a6474ab9d5f6c43
Ward keadan <73485499+wardkadan1@users.noreply.github.com>
2024-09-04 13:48:32 +0300

    Primitive-Types.2

---
+++ javascript/Primitive-Types.2/Primitive-Types.2.js
@@ -0,0 +1,26 @@
+//1
+
+const myName = "Ward Keadan";
+console.log(myName);
+
+//2
+
+const number1 = 10;
+const number2 = 20;
+const sum = number1 + number2;
+console.log(sum);
+
+//3
+
+const is_ward = true;
+console.log(is_ward);
+
+//4
+
+let undefinedV;
+console.log(undefinedV);
+
+//5
+
+let null_V = null;
+console.log(null_V);
