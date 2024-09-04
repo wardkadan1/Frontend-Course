446b3947cd7c9bf4ff0e8eb15f42e928dd1b4285
Ward keadan <73485499+wardkadan1@users.noreply.github.com>
2024-09-04 14:36:25 +0300

    Template_Literals.5

---
+++ javascript/Template_Literals.5/Template_Literals.5.js
@@ -0,0 +1,42 @@
+//1
+
+const name = "Ward";
+const age = 25;
+
+console.log(`My name is ${name} and I am ${age} years old.`);
+
+//2
+
+const length = 5;
+const width = 3;
+const area = length * width;
+
+console.log(
+  `The area of a rectangle with length of ${length} and width of ${width} is ${area}.`
+);
+
+//3
+
+const today = "Wednesday";
+
+const task1 = "run";
+const task2 = "play";
+const task3 = "sleep";
+
+console.log(`On ${today}, I will ${task1}, ${task2}, and ${task3}.`);
+
+//4
+
+const numberOfPets = 2;
+console.log(`I have ${numberOfPets} pets.`);
+
+//5
+
+const base = 5;
+const height = 10;
+
+const area1 = (base * height) / 2;
+
+console.log(
+  `The area of a triangle with base ${base} and height ${height} is ${area1}.`
+);
