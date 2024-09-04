8126f0681eb6963f5a9d1c4b2249f37a3c763803
Ward keadan <73485499+wardkadan1@users.noreply.github.com>
2024-09-04 13:55:44 +0300

    Variable Naming.3

---
+++ javascript/Variable Naming.3/Variable Naming.3.js
@@ -0,0 +1,17 @@
+//1
+
+const parisTemperature = 25;
+console.log(parisTemperature);
+
+//2
+
+const isUserLoggedIn = true;
+console.log(isUserLoggedIn);
+
+//3
+const customerEmail = "customer@gmail.com";
+console.log(customerEmail);
+
+//4
+const basketballScore = 95;
+console.log(basketballScore);
