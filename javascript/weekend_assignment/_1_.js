0d2a8d4d0d50c3558758d37b00d0e96193bf4c8c
Ward keadan <73485499+wardkadan1@users.noreply.github.com>
2024-09-16 01:31:16 +0300

    Ex2.7 EX3.1 EX3.2 EX4.1 EX4.2

--- javascript/weekend_assignment/_1_.js
+++ javascript/weekend_assignment/_1_.js
@@ -115,3 +115,123 @@ console.log(centuryFromYear(1));
 console.log(centuryFromYear(1705));
 console.log(centuryFromYear(1800));
 console.log("\n");
+
+//Ex2.7
+
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+    case "+":
+      return value1 + value2;
+    case "-":
+      return value1 - value2;
+    case "*":
+      return value1 * value2;
+    case "/":
+      return value1 / value2;
+    default:
+      return "";
+  }
+}
+
+console.log("\nEX2.7\n");
+console.log(basicOp("+", 4, 7));
+console.log(basicOp("-", 15, 18));
+console.log(basicOp("*", 5, 5));
+console.log(basicOp("/", 49, 7));
+console.log("\n");
+
+//Ex3.1
+
+function nb_year(p0, percent, aug, p) {
+  let population = p0;
+  for (let years = 0; ; years++) {
+    if (population >= p) return years;
+    population = population + population * (percent / 100) + aug;
+  }
+}
+
+console.log("\nEX3.1\n");
+console.log(nb_year(1000, 2, 50, 1200));
+console.log(nb_year(1500, 5, 100, 5000));
+console.log(nb_year(1500000, 2.5, 10000, 2000000));
+console.log("\n");
+
+//Ex3.2
+
+function numberOfPeople(busStops) {
+  let peopleOnBus = 0;
+  for (let i = 0; i < busStops.length; i++) {
+    let [peopleOn, peopleOff] = busStops[i];
+    peopleOnBus += peopleOn;
+    peopleOnBus -= peopleOff;
+  }
+  if (peopleOnBus < 0) return "wrong numbers";
+  return peopleOnBus;
+}
+
+console.log("\nEX3.2\n");
+console.log(
+  numberOfPeople([
+    [10, 0],
+    [3, 5],
+    [5, 8],
+  ])
+);
+console.log(
+  numberOfPeople([
+    [3, 0],
+    [9, 1],
+    [4, 10],
+    [12, 2],
+    [6, 1],
+    [7, 10],
+  ])
+);
+console.log(
+  numberOfPeople([
+    [3, 0],
+    [9, 1],
+    [4, 8],
+    [12, 2],
+    [6, 1],
+    [7, 30],
+  ])
+);
+console.log("\n");
+
+//Ex4.1
+
+function fibonacci(n) {
+  if (n === 0) return [];
+  if (n === 1) return [1];
+  if (n === 2) return [1, 1];
+  let fibarr = [1, 1];
+  for (let i = 2; i < n; i++) fibarr.push(fibarr[i - 1] + fibarr[i - 2]);
+  return fibarr;
+}
+
+console.log("\nEX4.1\n");
+console.log(fibonacci(10));
+console.log(fibonacci(8));
+console.log(fibonacci(5));
+console.log("\n");
+
+//Ex4.2
+
+function tribonacci(signature, n) {
+  if (n === 0) return [];
+  if (n <= 3) return signature.slice(0, n);
+  let result = signature.slice();
+  for (let i = 3; i < n; i++) {
+    let nextNumber = result[i - 1] + result[i - 2] + result[i - 3];
+    result.push(nextNumber);
+  }
+  return result;
+}
+
+console.log("\nEX4.2\n");
+console.log(tribonacci([1, 1, 1], 10));
+console.log(tribonacci([0, 0, 1], 10));
+console.log(tribonacci([1, 1, 1], 3));
+console.log(tribonacci([1, 1, 1], 0));
+console.log("\n");
